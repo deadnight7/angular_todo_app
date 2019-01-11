e2dc6744033fd05db979f70d3c0b16eba1e75ebc
dead.night7@gmail.com <chirag.p@loginextsolutions.com>
2019-01-11 16:13:32 +0530

    Adding initial component Login

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  userName = 'dead.night7@gmail.com';
+  password = 'Test!123';
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
